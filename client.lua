ee561ec5409cec100022e18f1004729326add592
GeneralShark <austinhorton289@gmail.com>
2023-05-14 12:13:13 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,6 +5,7 @@ RegisterNetEvent('shark-leotools:checkTint')
 AddEventHandler('shark-leotools:checkTint', function(entityID)
     if entityID ~= nil then
         local tintLev = GetVehicleWindowTint(entityID)
+        --print(tintLev)
         local tintDesc = ''..Config.tintTable[tintLev]..'% Tint Level'
         lib.notify({
             id = 'tintInfo',
@@ -82,6 +83,7 @@ local function ensureAnimDict(animDict)
         RequestAnimDict(animDict)
         while not HasAnimDictLoaded(animDict) do
             Wait(0)
+            --print('wait '..animDict)
         end
     end
     return animDict
